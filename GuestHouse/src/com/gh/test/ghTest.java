749c49020ee564b1c07aa404f387ccbd5dd6044a
Niftrone <128668486+Niftrone@users.noreply.github.com>
2025-06-04 14:36:58 +0900

    merge

--- GuestHouse/src/com/gh/test/ghTest.java
+++ GuestHouse/src/com/gh/test/ghTest.java
@@ -17,12 +17,12 @@ public class ghTest {
 
 	public static void main(String[] args) {
 		
-		ghDAOImpl ghservice = ghDAOImpl.getInstance();
+		ghDAOImpl gh = ghDAOImpl.getInstance();
 		//String uId, String name, String phNum, LocalDate birthday, String gender
 		
 		// INSERT CHECK
 //		try {
-//			ghservice.insertCustomer(new Customer(
+//			gh.insertCustomer(new Customer(
 //					"c777","Kingstone","010-1111-2222",LocalDate.of(1996, 01, 13),"M"));
 //		} catch (Exception e) {
 //			System.out.println(e.getMessage());
@@ -31,7 +31,7 @@ public class ghTest {
 		
 		// Update CHECK
 		try {
-			ghservice.updateCustomer(new Customer(
+			gh.updateCustomer(new Customer(
 					"c777","Kingstone2","010-1111-3333",LocalDate.of(1996, 01, 13),"M"));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
