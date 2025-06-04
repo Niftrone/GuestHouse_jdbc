3061e104c77fd468c7e9f98c084066d66e09f0b5
Niftrone <128668486+Niftrone@users.noreply.github.com>
2025-06-04 15:30:48 +0900

    Feat Reservation
    getReservation

--- GuestHouse/src/com/gh/test/ghTest.java
+++ GuestHouse/src/com/gh/test/ghTest.java
@@ -1,11 +1,8 @@
 package com.gh.test;
 
-import java.sql.SQLException;
 import java.time.LocalDate;
 
 import com.gh.dao.impl.ghDAOImpl;
-import com.gh.exception.DMLException;
-import com.gh.exception.DuplicateIDException;
 import com.gh.vo.Customer;
 import com.gh.vo.GuestHouse;
 import com.gh.vo.Reservation;
@@ -28,56 +25,62 @@ public class ghTest {
 //		}
 		
 		// Update CHECK
-		try {
-			gh.updateCustomer(new Customer(
-					"c777","Kingstone2","010-1111-3333",LocalDate.of(1996, 01, 13),"M"));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			
-		}
+//		try {
+//			gh.updateCustomer(new Customer(
+//					"c777","Kingstone2","010-1111-3333",LocalDate.of(1996, 01, 13),"M"));
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//			
+//		}
+//		
+//		try {
+//			gh.insertGH(new GuestHouse("GH010", "윤슬하우스", "Busan"));
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		try {
+//			gh.insertGH(new GuestHouse("GH011", "윤슬하우스", "Busan"));
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//
+//		try {
+//			gh.updateGH(new GuestHouse("GH022", "윤슬하우스2", "Busan"));
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		try {
+//			gh.updateGH(new GuestHouse("GH011", "윤슬하우스", "Seoul"));
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+		
+//		try {
+//			gh.insertReservation(new Reservation(
+//					"RV081", LocalDate.of(2025, 06, 12), LocalDate.of(2025, 06, 15), 2, 
+//					new Room("RM001",
+//							new GuestHouse("GH001", "소담하우스", "Seoul")
+//							,"햇살방", "F", 55000, 2),
+//					new Customer("C035", "임재현","01011110035",  LocalDate.of(1988,10,15), "M")
+//					));
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());	
+//		}
+
+//		try {
+//			gh.deleteGH("GH022");
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//		try {
+//			gh.deleteGH("GH011");
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
 		
 		try {
-			gh.insertGH(new GuestHouse("GH010", "윤슬하우스", "Busan"));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		try {
-			gh.insertGH(new GuestHouse("GH011", "윤슬하우스", "Busan"));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-
-		try {
-			gh.updateGH(new GuestHouse("GH022", "윤슬하우스2", "Busan"));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		try {
-			gh.updateGH(new GuestHouse("GH011", "윤슬하우스", "Seoul"));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		try {
-			gh.insertReservation(new Reservation(
-					"RV081", LocalDate.of(2025, 06, 12), LocalDate.of(2025, 06, 15), 2, 
-					new Room("RM001",
-							new GuestHouse("GH001", "소담하우스", "Seoul")
-							,"햇살방", "F", 55000, 2),
-					new Customer("C035", "임재현","01011110035",  LocalDate.of(1988,10,15), "M")
-					));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());	
-		}
-
-		try {
-			gh.deleteGH("GH022");
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		try {
-			gh.deleteGH("GH011");
-		} catch (Exception e) {
+			System.out.println(gh.getReservation("C010"));
+		}catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
